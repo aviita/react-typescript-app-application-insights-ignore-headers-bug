031ab8d5ceb5786d121a750931fd1a87a941008c
Antti Viita <antti.viita@vividworks.com>
2024-04-26 11:23:38 +0300

    App insights SDK initialization
    Added `telemetry.ts`, which reproduces the issue:
    > Object literal may only specify known properties, and 'ignoreHeaders' does not exist in type 'IConfiguration & IConfig'.ts(2353)
    
    Run build or just see the eslint highlight from the file.
    
    Also installed latest `@microsoft/applicationinsights-web` and `@microsoft/applicationinsights-react-js`.

---
+++ src/telemetry.ts
@@ -0,0 +1,54 @@
+import {
+  ApplicationInsights,
+  ITelemetryItem,
+} from "@microsoft/applicationinsights-web";
+import { ReactPlugin } from "@microsoft/applicationinsights-react-js";
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
+
+const reactPlugin = new ReactPlugin();
+
+const appInsights = new ApplicationInsights({
+  config: {
+    connectionString: process.env.VITE_APP_AI_INSTRUMENTATION_CONNECTION_STRING,
+    disableFetchTracking: false,
+    //enableRequestHeaderTracking: true, // Enable to get request headers in dependency logs
+    enableResponseHeaderTracking: true, // Enable to get response headers in dependency logs
+    ignoreHeaders: [
+      "Authorization",
+      "X-API-Key",
+      "WWW-Authenticate",
+      "date",
+      "x-ms-blob-type",
+      "x-ms-lease-status",
+      "x-ms-request-id",
+      "x-ms-version",
+      "content-type",
+      "X-Powered-By",
+      "X-Firefox-Spdy",
+      "Set-Cookie",
+      "Request-Context",
+      "Content-Encoding",
+      "Access-Control-Allow-Origin",
+      "Content-Encoding",
+      "Vary",
+    ], // Ignore these headers in dependency logs
+    disableAjaxTracking: false,
+    enableCorsCorrelation: true,
+    extensions: [reactPlugin /* debugPluginInstance */],
+    extensionConfig: {
+      [reactPlugin.identifier]: {
+        history,
+      },
+      /*
+        [DebugPlugin.identifier]: {
+          trackers: toTrack
+        }
+        */
+    },
+  },
+});
+
+appInsights.loadAppInsights();
+export { reactPlugin, appInsights };
